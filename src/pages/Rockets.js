47360ff7e919a778ecce191e837d234e03de07a4
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-17 02:16:32 +0530

    Add bootstrap to project

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
+  const dispatch = useDispatch();
+  const rocketsData = useSelector((state) => state.rockets.rockets);
+  const rocketsStatus = useSelector((state) => state.rockets.status);
+  const rocketsError = useSelector((state) => state.rockets.error);
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  console.log('data', rocketsData);
+
+  if (rocketsStatus === 'loading') {
+    return (
+      <h1 style={{ marginLeft: '40px' }}>Loading...</h1>
+    );
+  }
+
+  if (rocketsError !== null) {
+    return (
+      <h1 style={{ marginLeft: '40px' }}>{rocketsError}</h1>
+    );
+  }
   return (
     <div>Rockets</div>
   );
