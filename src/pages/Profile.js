a1658a2fd903f67a81cd43ccfb6edccd7f7dd816
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-18 15:07:43 +0530

    get reservedRocket

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bookedRockets } from '../redux/rockets/rocketsSlice';
 
 function Profile() {
+  const reservedRockets = useSelector((store) => store.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(bookedRockets());
+  }, [dispatch]);
+  console.table(reservedRockets);
   return (
     <div>Profile</div>
   );
