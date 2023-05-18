e4cb44fbe63bd47ca83a422ef5b40102570293a0
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-18 15:40:48 +0530

    Render reserved rockets

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { bookedRockets } from '../redux/rockets/rocketsSlice';
+import RocketCard from '../components/RocketCard';
 
 function Profile() {
   const reservedRockets = useSelector((store) => store.rockets);
@@ -9,9 +10,23 @@ function Profile() {
   useEffect(() => {
     dispatch(bookedRockets());
   }, [dispatch]);
-  console.table(reservedRockets);
+
   return (
-    <div>Profile</div>
+    <>
+      <h1 className="container">Booked Rockets</h1>
+      {reservedRockets.reservedRockets.length !== 0 ? '' : <p className="container">Please Book some Rockets</p>}
+      {reservedRockets.reservedRockets?.map((item) => (
+        <RocketCard
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          description={item.description}
+          flickrImages={item.image}
+          reserved={item.reserved}
+        />
+      ))}
+      ;
+    </>
   );
 }
 
