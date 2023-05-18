171c435a01cb175eda6367513d92abec30db2df8
Peter Akhigbe <akhigbe37@gmail.com>
2023-05-18 18:46:00 +0100

    Display joined mission in profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const missionsData = useSelector((state) => state.missions.missions);
+  const joinedMissions = missionsData.filter((mission) => mission.joined);
+
+  const profileStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    margin: '20px auto',
+  };
+
+  const ulStyles = {
+    listStyleType: 'none',
+    padding: '0',
+    margin: '0',
+  };
+
+  const listStyles = {
+    border: '1px solid lightgrey',
+    padding: '10px 25vw 10px 20px',
+  };
+
   return (
-    <div>Profile</div>
+    <div style={profileStyle} className="profile">
+      <div className="mission">
+        <h2 className="mission-h1">My Missions</h2>
+        <ul style={ulStyles} className="mission-ul">
+          { joinedMissions.map((mission) => (
+            <li style={listStyles} className="mission-li" key={mission.mission_id}>{ mission.mission_name }</li>
+          )) }
+        </ul>
+      </div>
+
+      <div>
+        <h2>My Rockets</h2>
+      </div>
+    </div>
   );
 }
 
