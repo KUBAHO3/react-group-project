637b41e7e43716cf5c76ab019eb6bf24e2eb786a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-19 01:46:15 +0530

    Add necessary tests

---
+++ src/tests/navbar.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Navbar from '../components/Navbar';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('Navbar component', () => {
+  it('renders without crashing', () => {
+    shallow(<Navbar />);
+  });
+
+  it('contains three navigation links', () => {
+    const wrapper = shallow(<Navbar />);
+    expect(wrapper.find('NavLink')).toHaveLength(3);
+  });
+
+  it('contains the correct text for each navigation link', () => {
+    const wrapper = shallow(<Navbar />);
+    expect(wrapper.find('NavLink').at(0).text()).toEqual('Rockets');
+    expect(wrapper.find('NavLink').at(1).text()).toEqual('Missions');
+    expect(wrapper.find('NavLink').at(2).text()).toEqual('Profile');
+  });
+});
