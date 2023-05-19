875729c331dce95dddc33e094b08918323632a05
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-05-19 14:26:52 +0530

    Resolve packages errors

--- src/tests/RocketCard.test.js
+++ src/tests/RocketCard.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import RocketCard from '../components/RocketCard';
 
 Enzyme.configure({ adapter: new Adapter() });
